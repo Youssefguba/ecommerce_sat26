a6d6ed2a6e1cef3ce8b0bf0eb56e8a22b3b81873
Youssefguba <Youssef.guba@abwaab.me>
2023-10-07 11:43:04 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -124,3 +124,12 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
